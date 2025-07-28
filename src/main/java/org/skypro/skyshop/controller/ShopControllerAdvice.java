45134c3c9b16ffedb5ead6a6d785b2a00ed13918
Slashaershov <127024236+Slashaershov@users.noreply.github.com>
2025-07-28 23:01:10 +0300

    minor fix

--- src/main/java/org/skypro/skyshop/controller/ShopControllerAdvice.java
+++ src/main/java/org/skypro/skyshop/controller/ShopControllerAdvice.java
@@ -4,10 +4,11 @@ import org.skypro.skyshop.model.exceptions.NoSuchProductException;
 import org.skypro.skyshop.model.exceptions.ShopError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@ControllerAdvice
 public class ShopControllerAdvice {
 
   @ExceptionHandler(NoSuchProductException.class)
